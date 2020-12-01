080800a8069f85526ba29a04a35ea684bcb007c7
Hugo Rodriguez <hi.rodriguez1998@gmail.com>
2020-11-30 22:50:21 -0300

    Agregado los EndPoints correspondientes a la entidad Proveedor

---
+++ ProyectoFinalASP-MVC/Controllers/ProveedoresController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProyectoFinalASP_MVC.Models;
+
+namespace ProyectoFinalASP_MVC.Controllers
+{
+    public class ProveedoresController : ApiController
+    {
+        private ComercialEntities db = new ComercialEntities();
+
+        // GET: api/Proveedores
+        public IQueryable<Proveedor> GetProveedor()
+        {
+            return db.Proveedor;
+        }
+
+        // GET: api/Proveedores/5
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult GetProveedor(int id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(proveedor);
+        }
+
+        // PUT: api/Proveedores/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProveedor(int id, Proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != proveedor.idProveedor)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(proveedor).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProveedorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Proveedores
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult PostProveedor(Proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Proveedor.Add(proveedor);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProveedorExists(proveedor.idProveedor))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = proveedor.idProveedor }, proveedor);
+        }
+
+        // DELETE: api/Proveedores/5
+        [ResponseType(typeof(Proveedor))]
+        public IHttpActionResult DeleteProveedor(int id)
+        {
+            Proveedor proveedor = db.Proveedor.Find(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            db.Proveedor.Remove(proveedor);
+            db.SaveChanges();
+
+            return Ok(proveedor);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProveedorExists(int id)
+        {
+            return db.Proveedor.Count(e => e.idProveedor == id) > 0;
+        }
+    }
+}
